6fa49038af0ef3a01960bb225c52a54a2e001042
kevin_000 <kdo94@student.cccd.edu>
2016-09-14 23:45:38 -0700

    Connected Model to View to Controller, still need to work on keyboard and final tounches on design

--- app/src/main/java/edu/orangecoastcollege/cs273/kdo94/shippingcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/shippingcalculator/MainActivity.java
@@ -1,13 +1,66 @@
 package edu.orangecoastcollege.cs273.kdo94.shippingcalculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+    private EditText weightEditText;
+    private TextView textBaseCost;
+    private TextView textAddedCost;
+    private TextView textTotalCost;
+
+    ShipItem currentPackage = new ShipItem();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weightEditText = (EditText) findViewById(R.id.weightEditText);
+        textBaseCost = (TextView) findViewById(R.id.textBaseCost);
+        textAddedCost = (TextView) findViewById(R.id.textAddedCost);
+        textTotalCost = (TextView) findViewById(R.id.textTotalCost);
+
+        weightEditText.addTextChangedListener(weightEditTextListener);
+        updateViews();
+    }
+
+    private TextWatcher weightEditTextListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(s.toString().isEmpty()){
+                currentPackage.setPackageWeight(0.0);
+            }
+            else
+            {
+                double amount = Double.parseDouble(s.toString());
+                currentPackage.setPackageWeight(amount);
+                updateViews();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private void updateViews(){
+        textAddedCost.setText(currency.format(currentPackage.getAddedCost()));
+        textBaseCost.setText(currency.format(currentPackage.getBaseCost()));
+        textTotalCost.setText(currency.format(currentPackage.getTotalShippingCost()));
     }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/kdo94/shippingcalculator/ShipItem.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/shippingcalculator/ShipItem.java
@@ -3,5 +3,66 @@ package edu.orangecoastcollege.cs273.kdo94.shippingcalculator;
 /**
  * Created by kevin_000 on 9/14/2016.
  */
+
 public class ShipItem {
+    private double mPackageWeight;
+    private double mAddedCost;
+    private double mTotalShippingCost;
+    private double BASE_COST = 3.0;
+    private double ADDED_COST = 0.5;
+    private int BASE_WEIGHT = 16;
+
+    public ShipItem() {
+        mAddedCost = 0.0;
+        mPackageWeight = 0.0;
+        mTotalShippingCost = 0.0;
+    }
+
+    public ShipItem(double mPackageWeight, double mAddedCost, double mTotalShippingCost) {
+        this.mPackageWeight = mPackageWeight;
+        this.mAddedCost = mAddedCost;
+        this.mTotalShippingCost = mTotalShippingCost;
+    }
+
+    public double getAddedCost() {
+        return mAddedCost;
+    }
+
+    public void setAddedCost(double mAddedCost) {
+        this.mAddedCost = mAddedCost;
+    }
+
+    public double getPackageWeight() {
+        return mPackageWeight;
+    }
+
+    public void setPackageWeight(double mPackageWeight) {
+        this.mPackageWeight = mPackageWeight;
+        recalculateAmount();
+    }
+
+    public double getTotalShippingCost() {
+        return mTotalShippingCost;
+    }
+
+    public void setTotalShippingCost(double mTotalShippingCost) {
+        this.mTotalShippingCost = mTotalShippingCost;
+    }
+
+    private void recalculateAmount(){
+        if(mPackageWeight <= 16){
+            mTotalShippingCost = BASE_COST;
+            mAddedCost = 0.0;
+        }
+        else
+        {
+            mAddedCost = Math.ceil((mPackageWeight - BASE_WEIGHT) / 4.0) * ADDED_COST;
+            mTotalShippingCost = BASE_COST + mAddedCost;
+        }
+    }
+
+    public double getBaseCost()
+    {
+        return BASE_COST;
+    }
 }
